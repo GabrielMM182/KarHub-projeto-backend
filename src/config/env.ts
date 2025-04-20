638f4403f6c26898ade955273383795800119d5a
GabrielMM182 <gmm_morais@hotmail.com>
2025-04-19 21:20:55 -0300

    fix: deploy missing env

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,3 +6,4 @@ export const MONGODB_URI = process.env.MONGODB_URI || '';
 export const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD || '';
 export const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID || '';
 export const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET || '';
+export const NODE_ENV = process.env.NODE_ENV || 'development';
